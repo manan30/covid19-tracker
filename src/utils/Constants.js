5fd8bf71e57fd36be9b900a2f992d5abbed85ca6
Manan Joshi <mananjoshi1995@gmail.com>
2020-03-24 09:43:07 -0400

    Updating map color palette

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -3,10 +3,16 @@ import { scaleLinear } from 'd3-scale';
 export const GEO_URL =
   'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
 
-export const colorScale = range =>
-  scaleLinear()
-    .domain([0, range])
-    .range(['#ffedea', '#ff5233']);
+export const colorScale = range => {
+  let ranges = new Array(5).fill(0);
+  const rangeCreator = Math.round(range / 5);
+
+  ranges = ranges.map((_, i) => rangeCreator * (i + 1));
+
+  return scaleLinear()
+    .domain([0, ...ranges])
+    .range(['#f7d9d9', '#e16968', '#d63230', '#9c2523', '#4e1312']);
+};
 
 const missingCountries = [
   'USA',
